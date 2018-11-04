79fd7856770edc664a7499488a64dcd6197c2f37
Christopher Pybus <cwpybus@gmail.com>
2018-11-04 16:41:40 -0500

    Added storyboards, login screens, signup screens, and main app navigator.

--- Bottled/ViewController.swift
+++ Bottled/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signinButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        signinButton.layer.cornerRadius = 10
+        signinButton.clipsToBounds = true
+        
+        signupButton.layer.cornerRadius = 10
+        signupButton.clipsToBounds = true
     }
 
     @IBAction func showMessage(sender: UIButton) {
